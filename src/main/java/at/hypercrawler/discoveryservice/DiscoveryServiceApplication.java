5ece2fb10a1ba25778b9a5e360500611c033d5a8
avollmaier <alois.vollmaier@gmail.com>
2023-04-12 20:08:05 +0200

    Initial commit

--- src/main/java/at/hypercrawler/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/at/hypercrawler/discoveryservice/DiscoveryServiceApplication.java
@@ -2,11 +2,13 @@ package at.hypercrawler.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class Application {
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(DiscoveryServiceApplication.class, args);
   }
 }
